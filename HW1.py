1e9855c99a10d0d557b75ef8d5911f0f610e53a3
Molokov-Klim <ultrakawaii9654449192@gmail.com>
2022-01-31 21:30:05 +0300

    HW1 complete

---
+++ HW1.py
@@ -0,0 +1,52 @@
+# 1) Создать переменную типа String
+StringX = "pi"
+# 2) Создать переменную типа Integer
+IntegerX = 3
+# 3) Создать переменную типа Float
+FloatX = 3.14
+# 4) Создать переменную типа Bytes
+BytesX = bytes([3])
+# 5) Создать переменную типа List
+ListX = [3,1,4]
+# 6) Создать переменную типа Tuple
+TupleX = (3,1,4)
+# 7) Создать переменную типа Set
+SetX = set('pi')
+# 8. Создать переменную типа Frozen set
+Frozen_setX = frozenset('pi')
+# 9) Создать переменную типа Dict
+DictX = {"1":"Сижу за решеткой в темнице сырой",
+"2":"Вскормленный в неволе орел молодой",
+"3":"Мой грустный товарищ, махая крылом",
+"4":"Кровавую пищу клюет под окном"
+}
+# 10) Вывести в консоль все выше перечисленные переменные с добавлением типа данных.
+print("String: ", StringX)
+print("Integer: ", IntegerX)
+print("Float: ", FloatX)
+print("Bytes: ", BytesX)
+print("List: ", ListX)
+print("Tuple: ", TupleX)
+print("Set: ", SetX)
+print("Frozen_set: ", Frozen_setX)
+
+String1 = "Pi "
+String2 = "3,14 "
+String3 = String1 + String2
+
+print("String3: ", String3)
+
+print(StringX, IntegerX)
+print(StringX + " " + str(IntegerX))
+
+
+
+
+
+
+# 11) Создать 2 переменные String, создать переменную в которой сканкатенируете эти переменные. Вывести в консоль.
+# 12) Вывести в одну строку переменные типа String и Integer используя “,” (Запятую)
+# 13) Вывести в одну строку переменные типа String и Integer используя “+” (Плюс)
+#
+
+
